c177715224ea66cb0fcdb19b8b0c5cf191752280
Daria Spodina <113624465+dspodina@users.noreply.github.com>
2022-10-13 16:17:48 +0200

    Add files via upload

---
+++ manager.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+
+let accessAllowed;
+let age = prompt('How old are you?', '');
+if (age > 18) {
+  accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
+alert(accessAllowed);
+
---
+++ manager.py
@@ -0,0 +1,5 @@
+age = input("Now old are you: ")
+if(int(age) > 18):
+    print ("accessAllowed");
+else:
+    print ("accessNotAllowed");
